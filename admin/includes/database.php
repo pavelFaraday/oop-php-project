26258d9eb38f8216bf21bc91b2d719cdfd49a338
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-21 21:01:01 +0400

    35. Improve connection to be more OOP

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -16,17 +16,19 @@ class Database
     public function open_db_connection()
     {
         // Connect to DB
-        $this->connection = mysqli_connect(DB_Host, DB_USER, DB_PASS, DB_NAME);
 
-        if (mysqli_connect_errno()) {
-            die("Database connection failed badly " . mysqli_error());
+        $this->connection = new mysqli(DB_Host, DB_USER, DB_PASS, DB_NAME);
+
+        if ($this->connection->connect_errno) {
+            die("Database connection failed badly " . $this->connection->connect_error);
         }
     }
 
     // 2. execute SQL queries against a MySQL database
     public function query($sql)
     {
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+        $this->confirm_query($result);
         return $result;
     }
 
@@ -34,15 +36,20 @@ class Database
     private function confirm_query($result)
     {
         if (!$result) {
-            die("Query Failed");
+            die("Query Failed " . $this->connection->error);
         }
     }
 
     // 4. escape special characters in a string to make it safe for insertion into a MySQL database using the MySQLi extension
     public function escape_string($string)
     {
-        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        $escaped_string = $this->connection->real_escape_string($string);
         return $escaped_string;
+    }
+
+    public function the_insert_id()
+    {
+        return $this->connection->insert_id;
     }
 }
 
