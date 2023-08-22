9710bee5d7cf80ef68b3d435dcb9d1cb89ce6b78
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-22 10:01:35 +0400

    37. Create User Class

---
+++ admin/includes/user.php
@@ -0,0 +1,11 @@
+<?php
+
+class User
+{
+    public function find_all_users()
+    {
+        global $database;
+        $result_set = $database->query("SELECT * FROM user");
+        return $result_set;
+    }
+}
