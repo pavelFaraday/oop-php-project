31cbba1cd6337cd052f41cc6c5383e1c57b42871
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-22 15:27:39 +0400

    43. Auto Instantiation Method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -19,13 +19,8 @@
 
             // Fetch specific user depending on id
             $found_user = User::find_user_by_id(1);
-            $user = new User();
 
-            $user->id = $found_user['id'];
-            $user->username = $found_user['username'];
-            $user->password = $found_user['password'];
-            $user->first_name = $found_user['first_name'];
-            $user->last_name = $found_user['last_name'];
+            $user = User::instantation($found_user);
 
             echo "<b>User ID: </b>" . $user->id . "<br>";
             echo "<b>Username: </b>" . $user->username . "<br>";
