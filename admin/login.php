9191fe66844c2fe85da12f530b5cd244ad13daf6
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-23 19:37:31 +0400

    personal updates

--- admin/login.php
+++ admin/login.php
@@ -1,11 +1,9 @@
 <?php
 require_once("includes/init.php");
 require_once("includes/header.php");
-
 if ($session->is_signed_in()) {
     redirect("index.php");
 }
-
 if (isset($_POST['submit'])) {
     $username = trim($_POST['username']);
     $password =  trim($_POST['password']);
