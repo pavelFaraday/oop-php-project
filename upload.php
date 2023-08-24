e460b0a3801c2d51c089a968cd562b4e44f9f8a9
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-24 13:02:13 +0400

    76. Moving Uploaded Files

--- upload.php
+++ upload.php
@@ -4,9 +4,22 @@
 <?php
 
 if (isset($_POST["submit"])) {
+    /*     
     echo "<pre>";
     print_r($_FILES["file_upload"]);
-    echo "<pre>";
+    echo "<pre>"; 
+    */
+
+    // access uploaded file data
+    $temp_name = $_FILES["file_upload"]["tmp_name"];
+    $the_file = $_FILES["file_upload"]["name"];
+    $directory = "uploads";
+    $the_message = "";
+    if (move_uploaded_file($temp_name, $directory . "/" . $the_file)) {
+        $the_message =  "file uploaded successfully";
+    }
+} else {
+    $the_message = "";
 }
 
 
@@ -20,6 +33,11 @@ if (isset($_POST["submit"])) {
 
 <body>
     <form action="upload.php" enctype="multipart/form-data" method="post">
+        <h2>
+            <?php
+            echo $the_message;
+            ?>
+        </h2>
         <input type="file" name="file_upload"><br>
         <input type="submit" name="submit">
     </form>
