b172505b7abb886e7a6208615eb00dd7a3838947
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-26 10:56:27 +0400

    103. create The Photo Class | Refactoring

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -10,28 +10,28 @@
             <?php
 
             // test if user is pulled from DB
-            /* $found_user = User::find_by_id(3);
+            $found_user = User::find_by_id(3);
             echo "<b>User ID: </b>" . $found_user->id . "<br>";
             echo "<b>Username: </b>" . $found_user->username . "<br>";
             echo "<b>First name: </b>" . $found_user->first_name . "<br>";
             echo "<b>Last name: </b>" . $found_user->last_name . "<br>";
-            echo "<hr>"; */
+            echo "<hr>";
 
             // Test if new user is INSERTed into DB
-            $user = new User();
+            /* $user = new User();
             $user->username = "1";
             $user->password = "2";
             $user->first_name = "3";
             $user->last_name = "4";
-            $user->create();
+            $user->create(); */
 
             // Test user UPDATE query
-            $user = User::find_by_id(79);
+            /* $user = User::find_by_id(79);
             $user->username = "PHP";
             $user->password = "H";
             $user->first_name = "P";
             $user->last_name = "❤️❤️";
-            $user->save();
+            $user->save(); */
 
             // Test SAVE method
             /*  $user->username = "Ludovico";
--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -4,16 +4,16 @@ class Db_object
 {
     public static function find_all()
     {
-        return static::find_this_query("SELECT * FROM " . static::$db_table . " ");
+        return static::find_by_query("SELECT * FROM " . static::$db_table . " ");
     }
 
     public static function find_by_id($user_id)
     {
-        $the_result_array = static::find_this_query("SELECT * FROM " . static::$db_table . " WHERE id=$user_id LIMIT 1");
+        $the_result_array = static::find_by_query("SELECT * FROM " . static::$db_table . " WHERE id=$user_id LIMIT 1");
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
 
-    public static function find_this_query($sql)
+    public static function find_by_query($sql)
     {
         global $database;
         $result_set = $database->query($sql);
@@ -44,5 +44,81 @@ class Db_object
 
         // check if array given key ($the_attribute) exists in current array ($object_properties)
         return array_key_exists($the_attribute, $object_properties);
+    }
+
+    protected function properties()
+    {
+        $properties = array();
+        foreach (static::$db_table_fields as $db_field) {
+            if (property_exists($this, $db_field)) {
+                $properties[$db_field] = $this->$db_field;
+            }
+        }
+        return $properties;
+    }
+
+    protected function clean_properties()
+    {
+        global $database;
+        $clean_properties = array();
+
+        foreach ($this->properties() as $key => $value) {
+            $clean_properties[$key] = $database->escape_string($value);
+        }
+        return $clean_properties;
+    }
+
+    // check if user ID exists in DB..
+    public function save()
+    {
+        return isset($this->id) ? $this->update() : $this->create();
+    }
+
+    // INSERT new User in DB
+    public function create()
+    {
+        global $database;
+        $properties = $this->clean_properties();
+
+        $sql = "INSERT INTO " . static::$db_table . "(" . implode(",", array_keys($properties)) . ")";
+        $sql .= "VALUES ('" . implode("','", array_values($properties)) . "')";
+
+        if ($database->query($sql)) {
+            $this->id = $database->insert_id();
+        } else {
+            return false;
+        }
+    }
+
+    // UPDATE User in DB
+    public function update()
+    {
+        global $database;
+        $properties = $this->clean_properties();
+        $properties_pairs = array();
+
+        foreach ($properties as $key => $value) {
+            $properties_pairs[] = "{$key}='{$value}'";
+        }
+
+        $sql = "UPDATE  " . static::$db_table . " SET ";
+        $sql .= implode(", ", $properties_pairs);
+        $sql .= " WHERE id = " . $database->escape_string($this->id);
+
+        $database->query($sql);
+        return (mysqli_affected_rows($database->connection) == 1) ?  true : false;
+    }
+
+    // DELETE User FROM DB
+    public function delete()
+    {
+        global $database;
+
+        $sql = "DELETE FROM  " . static::$db_table . " ";
+        $sql .= "WHERE id=" . $database->escape_string($this->id);
+        $sql .= " LIMIT 1";
+
+        $database->query($sql);
+        return (mysqli_affected_rows($database->connection) == 1) ?  true : false;
     }
 }
