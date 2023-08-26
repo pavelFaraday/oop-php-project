06deddca6cb1e212a70ec980d5c3ba5dc32e0ef7
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-26 12:45:17 +0400

    106. Setting Up PHOTO Properties Array

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -10,4 +10,20 @@ class Photo extends Db_object
     public $filename;
     public $type;
     public $size;
+
+
+    // create properties for File Upload
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors_array = array(
+        UPLOAD_ERR_OK => "There is Nor ERROR",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload max_ filesize directives",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX FILE SIZE directive",
+        UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded.",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder.",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload."
+    );
 }
