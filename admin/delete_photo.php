9e4a20a6451243a62453951fc1020dc8eedf06b6
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-28 19:17:00 +0400

    118. Customize Photo DELETE logic - Part 1

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -7,3 +7,18 @@ if (!$session->is_signed_in()) {
 }
 ?>
 
+<?php
+
+if (empty($_GET['photo_id'])) {
+    redirect("photos.php");
+}
+
+$photo = Photo::find_by_id($_GET['photo_id']);
+
+if ($photo) {
+    $photo->delete_photo();
+} else {
+    redirect("photos.php");
+}
+
+?>
