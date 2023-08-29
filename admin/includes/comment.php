36b19bdedd4016f37d94c75b0cc9f31421330814
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-29 21:38:53 +0400

    150. Self Instantiation Comment Method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -5,7 +5,23 @@ class Comment extends Db_object
     protected static $db_table = "comments";
     protected static $db_table_fields = array('id', 'photo_id', 'author', 'body');
     public $id;
-    public $author;
     public $photo_id;
+    public $author;
     public $body;
+
+
+    public static function create_comment($photo_id, $author = "John Doe", $body = "")
+    {
+        if (!empty($photo_id) && !empty($author) && !empty($body)) {
+            $comment = new Comment();
+
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
+
+            return $comment;
+        } else {
+            return false;
+        }
+    }
 }
