161e5939e2b7f11830fd6f0c90055aedd6cb7b7f
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-29 17:05:52 +0400

    133. Display User Page

--- admin/users.php
+++ admin/users.php
@@ -1,5 +1,18 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+// if user is not logged in -> redirection
+if (!$session->is_signed_in()) {
+    redirect("login.php");
+}
+?>
+
+<?php
+
+$users = User::find_all();
+
+?>
+
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -12,31 +25,59 @@
 </nav>
 
 <div id="page-wrapper">
-
     <div class="container-fluid">
-
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    USERS
+                    userS
                     <small>Subheading</small>
                 </h1>
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
-                    </li>
-                </ol>
+
+                <div class="col-md-12">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Photo</th>
+                                <th>Username</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php foreach ($users as $user) : ?>
+
+                                <tr>
+                                    <td><?php echo $user->id; ?></td>
+                                    <td>
+                                        <img src="<?php echo $user->user_image; ?>" width="100px" alt="">
+                                    </td>
+                                    <td><?php echo $user->username; ?>
+                                        <div class="action_links">
+                                            <a href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                            <a href="edit_user.php?id=<?php echo $user->id; ?>">Edit</a>
+                                            <a href="#">View</a>
+                                        </div>
+                                    </td>
+
+
+
+                                    <td><?php echo $user->first_name; ?></td>
+                                    <td><?php echo $user->last_name; ?></td>
+                                </tr>
+
+                            <?php endforeach; ?>
+
+                        </tbody>
+                    </table> <!--End of Table-->
+                </div>
             </div>
         </div>
         <!-- /.row -->
-
     </div>
     <!-- /.container-fluid -->
-
 </div>
 <!-- /#page-wrapper -->
 
