399a22d675055c270f2a0deef8101c43e8ec7f67
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-30 18:38:54 +0400

    181. Create Pagination class __construct Function

---
+++ admin/includes/paginate.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Paginate
+{
+
+    public $current_page;
+    public $items_per_page;
+    public $items_total_count;
+
+
+    public function __construct($page = 1, $items_per_page = 4, $items_total_count = 0)
+    {
+        $this->current_page = (int)$page;
+        $this->items_per_page = (int)$items_per_page;
+        $this->items_total_count = (int)$items_total_count;
+    }
+}
