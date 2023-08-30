25c1cc28a3b6f935c4d1b48064092988ec07b475
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-30 18:52:06 +0400

    184. finish Pagination Class

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -31,4 +31,19 @@ class Paginate
     {
         return ceil($this->items_total_count / $this->items_per_page);
     }
+
+    public function has_previous()
+    {
+        return $this->previous() >= 1 ? true : false;
+    }
+
+    public function has_next()
+    {
+        return $this->next() <= $this->page_total() ? true : false;
+    }
+
+    public function offset()
+    {
+        return ($this->current_page - 1) * $this->items_per_page;
+    }
 }
