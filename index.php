81ae5d00e056c0c7b0efb849feb16c7328f1e92d
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-30 19:39:31 +0400

    188. Paginate Indication and Looping

--- index.php
+++ index.php
@@ -37,6 +37,14 @@ $photos = Photo::find_by_query($sql);
                         echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
                     }
 
+                    for ($i = 1; $i <= $paginate->page_total(); $i++) {
+                        if ($i == $paginate->current_page) {
+                            echo  "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                        } else {
+                            echo  "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                        }
+                    }
+
                     if ($paginate->has_previous()) {
                         echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
                     }
