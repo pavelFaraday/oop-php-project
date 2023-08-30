8f42e15ae23a5d9b007a2066d5957dd02b5cc2c8
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-08-30 19:06:15 +0400

    184. Instantiating and Testing Paginate

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 include("includes/header.php");
 
 $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
-$items_per_page = 16;
+$items_per_page = 4;
 $items_total_count = Photo::count_all();
 
-$photos = Photo::find_all();
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+
+$sql = "SELECT * FROM photos ";
+$sql .= "LIMIT {$items_per_page} ";
+$sql .= "OFFSET {$paginate->offset()}";
+$photos = Photo::find_by_query($sql);
+
+
+// $photos = Photo::find_all();
 ?>
 
 <div class="row">
